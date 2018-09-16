42b8151084f33a21c79354dbfb8cd1b6528b43b9
maximilianopagani <maxipagani@hotmail.com>
2018-09-16 18:43:39 -0300

    Unos chequeos mas al inicializar. Sé que esta medio negro porque se puede modularizar mejor en funciones y eso, por ahora no dejo asi que no es muy largo, despues emrpolijo si hay tiempo, prefiero avanzar. En el chequeo de los ejecutables seguramente ahi si tenga que hacerlo

--- bin/initVars.sh
+++ bin/initVars.sh
@@ -3,32 +3,35 @@
 initVars() 
 {	
 	SCRIPTPATH="$( cd "$(dirname "$0")" ; cd .. ; pwd -P )" # A modificar cuando tengamos los scripts del principio
+	
+	INIT_SUCCESS=TRUE
 
 	echo
 
 	if [ "$TP_SISOP_INIT" = "YES" ] #Variable que vendría de más arriba
-
 	then
+		INIT_SUCCESS=FALSE
 		echo "Error: sistema ya inicializado."
 		echo
 		return 0
 	fi
 
-	if [ ! -f "$SCRIPTPATH/conf/tpconfig.txt" ] 
+	# Verifico existencia con permiso de lectura del archivo de configuración
 
+	if [ ! -f "$SCRIPTPATH/conf/tpconfig.txt" ] 
 	then
+		INIT_SUCCESS=FALSE
 		echo "Error: no existe el archivo de configuracion tpconfig.txt."
 		echo
 		return 0
 	else
-		if [ ! -r "$SCRIPTPATH/conf/tpconfig.txt" ]
-		
+		if [ ! -r "$SCRIPTPATH/conf/tpconfig.txt" ]		
 		then
 			chmod +r "$SCRIPTPATH/conf/tpconfig.txt"
 			
 			if [ ! -r "$SCRIPTPATH/conf/tpconfig.txt" ]
-		
 			then
+				INIT_SUCCESS=FALSE
 				echo "Error: el archivo de configuracion tpconfig.txt no tiene permiso de lectura."
 				echo
 				return 0
@@ -45,17 +48,73 @@ initVars()
 	while read VARIABLE DIRECCION
 
 	do	
-		export $VARIABLE=$DIRECCION
-		echo "Cargando: $VARIABLE - RUTA: $DIRECCION"
+		if [ ! -d "$DIRECCION" ]		
+		then
+			INIT_SUCCESS=FALSE
+			echo "Error al cargar $VARIABLE. El directorio no existe ($DIRECCION)"
+ 
+		else
+			export $VARIABLE=$DIRECCION
+			echo "Cargando: $VARIABLE - RUTA: $DIRECCION"
+		fi
 
 	done <$SCRIPTPATH/conf/tpconfig.txt
-
-	IFS=$IFS_BACKUP
 	
-	export TP_SISOP_INIT=YES
-
 	echo
-	echo "Sistema inicializado."
+	
+	IFS=$IFS_BACKUP
+
+	# Verifico existencia con permiso de lectura de los archivos maestros
+
+	if [ ! -f "$MAESTROSDIR/sucursales.txt" ]
+	then
+		INIT_SUCCESS=FALSE
+		echo "Error: no existe el archivo de maestros sucursales.txt."
+		echo
+	else
+		if [ ! -r "$MAESTROSDIR/sucursales.txt" ]		
+		then
+			chmod +r "$MAESTROSDIR/sucursales.txt"
+			
+			if [ ! -r "$MAESTROSDIR/sucursales.txt" ]		
+			then
+				INIT_SUCCESS=FALSE
+				echo "Error: el archivo de maestros sucursales.txt no tiene permiso de lectura."
+				echo
+			fi
+		fi	
+	fi
+
+	if [ ! -f "$MAESTROSDIR/operadores.txt" ]
+	then
+		INIT_SUCCESS=FALSE
+		echo "Error: no existe el archivo de maestros operadores.txt."
+		echo
+	else
+		if [ ! -r "$MAESTROSDIR/operadores.txt" ]	
+		then
+			chmod +r "$MAESTROSDIR/operadores.txt"
+			
+			if [ ! -r "$MAESTROSDIR/operadores.txt" ]		
+			then
+				INIT_SUCCESS=FALSE
+				echo "Error: el archivo de maestros operadores.txt no tiene permiso de lectura."
+				echo
+			fi
+		fi	
+	fi
+
+	# VERIFICAR QUE TODOS LOS EJECUTABLES DE /BIN TENGAN PERMISO DE EJECUCION
+	#########################################################################
+
+	if [ "$INIT_SUCCESS" = "TRUE" ]
+	then	
+		export TP_SISOP_INIT=YES
+		echo "Sistema inicializado con éxito."
+	else
+		echo "Error al inicializar el sistema."
+	fi
+
 	echo
 
 	return 1;
